09acc27159dabb48bcd692b2a8c01e68edee50a9
Jens Theisen <pass-github@zokfotpik.de>
2017-12-15 15:50:41 +0100

    Namespace change.

--- ServerSettings/EntryAssemblyAttribute.cs
+++ ServerSettings/EntryAssemblyAttribute.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace MonkeyBusters
+namespace IronStone
 {
     // From: https://stackoverflow.com/a/40269369/870815
 
--- ServerSettings/Settings.cs
+++ ServerSettings/Settings.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
 
-namespace MonkeyBusters.ServerSettings
+namespace IronStone.ServerSettings
 {
     public static class Settings
     {
